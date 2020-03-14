a416ef48b584d5eaf0919899feeb2193cc5143a4
thiagob <thiagob@gmail.com>
2020-03-14 10:13:52 -0300

    Improve library selection

--- config.py
+++ config.py
@@ -1,15 +1,41 @@
 import importlib
 import os
-import thiagob as lib
 
-# subfolders = [ f.path for f in os.scandir('.') if f.is_dir() ]
+import inspect
 
-# library = "template"
-# for f in subfolders:
-#     if not f.startswith(".\."):
-#         folder = f[2:]
-#         if folder not in ["docs", "exercises", "template", "tests"]:
-#             library = folder
-#             print("SELECT LIBRABRY >>> " + library)
+def in_unit_test():
+  current_stack = inspect.stack()
+  for stack_frame in current_stack:
+    for program_line in stack_frame[4]:    # This element of the stack frame contains 
+      if "unittest" in program_line:       # some contextual program lines
+        return True
+  return False
 
-# lib = importlib.import_module(library)
+folders = [ f.name for f in os.scandir('.') if f.is_dir()]
+folders = [f for f in folders if f[0] not in ['.', '_']]
+folders.remove('tests')
+folders.remove('docs')
+
+# Inform the default library you want to use
+library = "template" # <<<<<<<<
+if len(folders) == 2:
+    library = "thiagob"
+else:
+    folders.remove('template')
+    folders.remove('thiagob')
+
+    library = folders[0]
+
+# elif not in_unit_test():
+#     print("Which library do you want to import?")
+#     for idx, val in enumerate(folders):
+#         print(" Press %i for %s" % (idx, val))
+#     n = int(input("Number: "))
+
+#     library = folders[n]
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("from " + library + " import lib")
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
+lib = importlib.import_module(library)
