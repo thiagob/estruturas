87c5bc57619d8be09b1db22dac90823762526e00
thiagob <thiagob@gmail.com>
2020-03-30 20:36:18 -0300

    Simplify double linked list example

--- thiagob/exercises/lesson_05/examples/03_double_linked_list.py
+++ thiagob/exercises/lesson_05/examples/03_double_linked_list.py
@@ -1,15 +1,20 @@
 class Node:
 
-    def __init__(self, text, next):
+    def __init__(self, text):
         self.previous = None
+        self.next = None
         self.text = text
-        self.next = next
-        if next:
-            self.next.previous = self 
 
-n1 = Node("Bohn", None)
-n2 = Node("Thiago", n1)
-n3 = Node("Professor", n2)
+first = Node("Professor")
+middle = Node("Thiago")
+last = Node("Bohn")
 
-print("Node: %s | Previous: %s | Next: %s" % (n2.text, n2.previous.text, n2.next.text))
+first.next = middle
+middle.next = last
+
+last.previous = middle
+middle.previous = first
+
+print("Midle: %s | Previous: %s | Next: %s" % \
+     (middle.text, middle.previous.text, middle.next.text))
 # Node: Thiago | Previous: Professor | Next: Bohn
